338488452df819ce464abec75f3551832666a7a5
Fred Duarte <fduartej@gmail.com>
2024-09-06 22:04:14 -0500

    Agregando Contacto

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using app2game.Data;
+using app2game.Models;
+
+namespace app2game.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public ContactoController(ILogger<ContactoController> logger,ApplicationDbContext context)
+        {
+            _logger = logger;
+             _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Enviar(Contacto objcontato)
+        {
+            _logger.LogDebug("Ingreso a Enviar Mensaje");
+            
+            _context.Add(objcontato);
+            _context.SaveChanges();
+
+            ViewData["Message"] = "Se registro el contacto";            
+            
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
