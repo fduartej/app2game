16a4af8663cb33e700a7467978bf4ba12ea76dac
Fred Duarte <fduartej@gmail.com>
2024-10-11 21:05:56 -0500

    fix response

--- Integration/CurrencyExchange/dto/CurrencyExchangeResponse.cs
+++ Integration/CurrencyExchange/dto/CurrencyExchangeResponse.cs
@@ -1,27 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Humanizer;
-using NuGet.Packaging.Signing;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace app2game.Integration.CurrencyExchange.dto
 {
     public class CurrencyExchangeResponse
     {
         public bool success { get; set; }
-        public string date { get; set; }
+         
+        [JsonPropertyName("date")]
+        public string? dateExchange { get; set; }
         public decimal result { get; set; }
-        public Info info { get; set; }
+        public Info? info { get; set; }
 
-        public CurrencyExchangeResponse(){
-            info = new Info();
-        }
     }
 
     public class Info
     {
-        public decimal timestamp { get; set; }
+        [JsonPropertyName("timestamp")]
+        public decimal timestampRate { get; set; }
         public decimal rate { get; set; }
 
     }
